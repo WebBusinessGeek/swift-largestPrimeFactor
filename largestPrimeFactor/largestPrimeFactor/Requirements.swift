632024b984558b94a8aa91dc604bd7531c333753
Kevin Washington <kevw12188@gmail.com>
2015-09-04 16:14:11 -0400

    first commit

---
+++ largestPrimeFactor/largestPrimeFactor/Requirements.swift
@@ -0,0 +1,20 @@
+//
+// Created by Kev Washington on 9/4/15.
+// Copyright (c) 2015 kw. All rights reserved.
+//
+
+import Foundation
+
+class Requirements {
+
+    /*
+
+    The prime factors of 13195 are 5, 7, 13 and 29.
+
+    What is the largest prime factor of the number 600851475143?
+
+        - Get the prime factors of Integer
+        - Get the highest interger value in a data collection
+
+    */
+}
